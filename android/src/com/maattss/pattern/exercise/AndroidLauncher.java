66f33fc84a799a672f5129538a9bc134de967773
Mats Tyldum <ma.tyldum@gmail.com>
2020-01-27 20:14:10 +0100

    First commit

---
+++ android/src/com/maattss/pattern/exercise/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.maattss.pattern.exercise;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.maattss.pattern.exercise.PatternExercise;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PatternExercise(), config);
+	}
+}
---
+++ desktop/src/com/maattss/pattern/exercise/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.maattss.pattern.exercise.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.maattss.pattern.exercise.PatternExercise;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PatternExercise(), config);
+	}
+}
